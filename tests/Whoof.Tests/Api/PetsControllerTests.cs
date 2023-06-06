c55ece1d73d5acf3e91b6d24ae05d231c2fae0ab
Guilherme Raduenz <graduenz@gmail.com>
2023-06-06 08:47:01 -0300

    test: added tests for adding pets

--- tests/Whoof.Tests/Api/PetsControllerTests.cs
+++ tests/Whoof.Tests/Api/PetsControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http.Json;
 using FluentAssertions;
 using Whoof.Api.Entities;
+using Whoof.Api.Enums;
 using Whoof.Api.Models;
 using Whoof.Tests.Api.Support;
 
@@ -42,5 +43,59 @@ public class PetsControllerTests : BaseControllerTests
             .NotBeNull().And
             .BeEquivalentTo(expectedPet, c => c
                 .Excluding(m => m.Vaccinations));
+    }
+
+    [Fact]
+    public async Task AddOneAsync_WithValidFields_AddsAsExpected()
+    {
+        // Arrange
+        var pet = new Pet
+        {
+            Name = "Ravena",
+            PetType = PetType.Dog,
+            OwnerId = Guid.NewGuid()
+        };
+        
+        // Act
+        var response = await HttpClient.PostAsJsonAsync("v1/pets/", pet);
+        
+        // Assert
+        response.Should()
+            .NotBeNull().And
+            .Be201Created().And
+            .BeAs(pet, c => c
+                .Excluding(m => m.Id));
+    }
+
+    public static IEnumerable<object[]> _AddOneAsync_WithInvalidData_DoesntAdd()
+    {
+        yield return new object[] {
+            new Pet { Name = "", PetType = PetType.Capybara },
+            new[] { "'Name' must not be empty." }
+        };
+        
+        yield return new object[] {
+            new Pet { Name = "Qwerty", PetType = PetType.Unspecified },
+            new[] { "'Pet Type' must not be equal to 'Unspecified'." }
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(_AddOneAsync_WithInvalidData_DoesntAdd))]
+    public async Task AddOneAsync_WithInvalidData_DoesntAdd(Pet pet, string[] expectedErrors)
+    {
+        // Act
+        var response = await HttpClient.PostAsJsonAsync("v1/pets/", pet);
+        
+        // Assert
+        response.Should()
+            .NotBeNull().And
+            .Be400BadRequest();
+
+        var result = await response.Content.ReadFromJsonAsync<ValidationErrorsResult>();
+        result.Should().NotBeNull();
+        result.Type.Should().Be("VALIDATION_ERRORS");
+        result.Errors.Select(m => m.ErrorMessage).Should()
+            .BeEquivalentTo(expectedErrors);
     }
 }
