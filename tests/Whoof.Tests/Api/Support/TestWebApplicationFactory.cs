a8a4e067193aed20767a27925ba23d6f66a64e9f
Guilherme Raduenz <graduenz@gmail.com>
2023-07-19 11:11:07 -0300

    feat: introduced basic sonarcloud action

--- tests/Whoof.Tests/Api/Support/TestWebApplicationFactory.cs
+++ tests/Whoof.Tests/Api/Support/TestWebApplicationFactory.cs
@@ -12,10 +12,12 @@ public class TestWebApplicationFactory<TProgram>
     : WebApplicationFactory<TProgram> where TProgram : class
 {
     private readonly string _exclusiveDbName;
+    private readonly bool _isSonar;
 
     public TestWebApplicationFactory(string exclusiveDbName)
     {
         _exclusiveDbName = exclusiveDbName;
+        _isSonar = Environment.GetEnvironmentVariable("IS_SONAR")?.Equals("1") ?? false;
     }
     
     protected override void ConfigureWebHost(IWebHostBuilder builder)
@@ -42,13 +44,23 @@ public class TestWebApplicationFactory<TProgram>
                  typeof(DbContextOptions<AppDbContext>));
 
         services.Remove(dbContextDescriptor);
-
-        var connstrBuilder = new DbConnectionStringBuilder();
-        connstrBuilder.ConnectionString = configuration["ConnectionStrings:AppDbContext"];
-        connstrBuilder["Database"] = _exclusiveDbName;
         
         services.AddDbContext<AppDbContext>(options =>
-            options.UseNpgsql(connstrBuilder.ConnectionString)
-        );
+        {
+            // TODO: Change to always use PostgreSQL
+            if (_isSonar)
+            {
+                // Tests with memory db when running tests
+                options.UseInMemoryDatabase(_exclusiveDbName);
+            }
+            else
+            {
+                // Test with postgres when running tests locally
+                var connstrBuilder = new DbConnectionStringBuilder();
+                connstrBuilder.ConnectionString = configuration["ConnectionStrings:AppDbContext"];
+                connstrBuilder["Database"] = _exclusiveDbName;
+                options.UseNpgsql(connstrBuilder.ConnectionString);
+            }
+        });
     }
 }
