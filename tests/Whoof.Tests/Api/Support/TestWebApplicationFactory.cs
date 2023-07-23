0e4dbdb8a39752e697b174a4452f97cf38ecd961
Guilherme Raduenz <graduenz@gmail.com>
2023-07-23 17:09:14 -0300

    chore: improved test action to use docker compose dependencies

--- tests/Whoof.Tests/Api/Support/TestWebApplicationFactory.cs
+++ tests/Whoof.Tests/Api/Support/TestWebApplicationFactory.cs
@@ -12,12 +12,10 @@ public class TestWebApplicationFactory<TProgram>
     : WebApplicationFactory<TProgram> where TProgram : class
 {
     private readonly string _exclusiveDbName;
-    private readonly bool _isSonar;
 
     public TestWebApplicationFactory(string exclusiveDbName)
     {
         _exclusiveDbName = exclusiveDbName;
-        _isSonar = Environment.GetEnvironmentVariable("IS_SONAR")?.Equals("1") ?? false;
     }
     
     protected override void ConfigureWebHost(IWebHostBuilder builder)
@@ -47,20 +45,10 @@ public class TestWebApplicationFactory<TProgram>
         
         services.AddDbContext<AppDbContext>(options =>
         {
-            // TODO: Change to always use PostgreSQL
-            if (_isSonar)
-            {
-                // Tests with memory db when running tests
-                options.UseInMemoryDatabase(_exclusiveDbName);
-            }
-            else
-            {
-                // Test with postgres when running tests locally
-                var connstrBuilder = new DbConnectionStringBuilder();
-                connstrBuilder.ConnectionString = configuration["ConnectionStrings:AppDbContext"];
-                connstrBuilder["Database"] = _exclusiveDbName;
-                options.UseNpgsql(connstrBuilder.ConnectionString);
-            }
+            var connstrBuilder = new DbConnectionStringBuilder();
+            connstrBuilder.ConnectionString = configuration["ConnectionStrings:AppDbContext"];
+            connstrBuilder["Database"] = _exclusiveDbName;
+            options.UseNpgsql(connstrBuilder.ConnectionString);
         });
     }
 }
