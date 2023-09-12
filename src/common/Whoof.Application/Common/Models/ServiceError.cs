f2b29f2c45cc473128fd2403430a5afe8fa751d2
Guilherme Raduenz <graduenz@gmail.com>
2023-09-11 22:26:00 -0300

    style: changed to target-typed new ctor

--- src/common/Whoof.Application/Common/Models/ServiceError.cs
+++ src/common/Whoof.Application/Common/Models/ServiceError.cs
@@ -11,13 +11,13 @@ public class ServiceError : IEqualityComparer<ServiceError>
     public string Message { get; }
     public int Code { get; }
     
-    public static ServiceError DefaultError => new ServiceError(999, "An exception occured.");
-    public static ServiceError CustomMessage(string errorMessage) => new ServiceError(998, errorMessage);
-    public static ServiceError ForbiddenError => new ServiceError(997, "You are not authorized to call this action.");
-    public static ServiceError Cancelled => new ServiceError(996, "The request has been cancelled.");
-    public static ServiceError NotFound => new ServiceError(995, "The specified resource was not found.");
-    public static ServiceError Validation => new ServiceError(994, "One or more validation errors occurred.");
-    public static ServiceError InternalServerError => new ServiceError(500, "Internal server error.");
+    public static ServiceError DefaultError => new(999, "An exception occured.");
+    public static ServiceError CustomMessage(string errorMessage) => new(998, errorMessage);
+    public static ServiceError ForbiddenError => new(997, "You are not authorized to call this action.");
+    public static ServiceError Cancelled => new(996, "The request has been cancelled.");
+    public static ServiceError NotFound => new(995, "The specified resource was not found.");
+    public static ServiceError Validation => new(994, "One or more validation errors occurred.");
+    public static ServiceError InternalServerError => new(500, "Internal server error.");
 
     public override int GetHashCode()
     {
